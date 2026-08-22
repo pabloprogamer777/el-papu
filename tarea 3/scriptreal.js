08eb7885c86c168c2740fbe13cf25d74a1536459
pabloprogamer777 <pablo.tomas.olivares@gmail.com>
2026-08-21 21:05:22 -0400

    traspaso del primer script + ver la última compra (artículo ingresado al carrito)

--- tarea 3/scriptreal.js
+++ tarea 3/scriptreal.js
@@ -1,1 +1,15 @@
+let CompraFinal = null;
+let TotalDeCompra = 0;
 
+function Comprar(Productname) {
+  ultimoElemento = Productname;
+  TotalDeCompra++;
+  document.getElementByID('ContadorDelCarro').textContent = TotalDeCompra;
+}
+function VerUltimaCompra(){
+  if(ultimoElemento == null){
+    alert('Agregue algo al carrito para ver su último artículo,');
+  } else {
+    alert('Su último artículo añadido al carrito es: ' +ultimoElemento);
+  }
+}
