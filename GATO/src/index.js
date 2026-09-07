08cd755bdf5adf799731ccf243a4e7ca35f74c82
pabloprogamer777 <pablo.tomas.olivares@gmail.com>
2026-09-06 23:46:44 -0300

    Change import path for App component to app.js

--- GATO/src/index.js
+++ GATO/src/index.js
@@ -2,4 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
 
-import App from './App';
+import App from './app.js';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
